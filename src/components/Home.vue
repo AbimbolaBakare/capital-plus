ab57f471348250355a22ef35f2fdf6ce35c62e00
Bimm <deomolara@yahoo.com>
2020-04-03 17:51:00 +0100

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>  
+    <div>
+        <!-- START PRELOADER -->
+	<div id="preloader">
+		<div id="status">&nbsp;</div>
+	</div>
+	<!-- END PRELOADER -->
+	<!-- Return to Top -->
+	<a href="javascript:" id="return-to-top"><span class="fa fa-arrow-up" aria-hidden="true"></span></a>
+	<!-- navigation -->
+	<!-- start Fixed navbar -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for mobile display -->
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#about">
+					<img alt="Marina Marques" class="navbar-brand" src="http://marinamarques.pt/img/marina_marques_logo.png">
+				</a>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#ResponsiveNav">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-right" id="ResponsiveNav">
+				<ul class="nav navbar-nav">
+					<li><a href="#about-me" class="page-scroll">ABOUT</a></li>
+					<li><a href="#portfolio" class="page-scroll">PORTFOLIO</a></li>
+					<li><a href="#services" class="page-scroll">SERVICES</a></li>
+					<li><a href="#contact" class="page-scroll">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- end container fluid -->
+	</nav>
+	<!-- end navigation -->
+	<!-- intro header -->
+	<header class="intro">
+		<div class="intro-body">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<!-- start typer hello  -->
+						<div class="brand-heading">
+							<span class="txt-rotate" data-period="100" data-rotate='[ "Welcome to", "Capital", "Plus", "Limited" ]'></span>
+						</div>
+						<!-- end typer hello -->
+						<h2 class="intro-text"> AGRO ALLIED <br> SUPPLIES AND RESOURCES</h2>
+					</div>
+					<!-- end column wrapping -->
+				</div>
+				<!-- end row header -->
+			</div>
+			<!-- end container header -->
+		</div>
+		<!-- end intro body -->
+	</header>
+	<!-- end intro header -->
+	<!-- <div id="mouse-here"></div> -->
+	<!-- only for mouse to fall before the start of the section -->
+	<!-- start about me -->
+    </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from "../components/Home"
+
+Vue.use(Router);
+
+export default new Router({
+linkActiveClass : true,
+ mode: 'history',
+base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        
+
+    ]
+})
